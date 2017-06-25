c679f0b60ae887e3ac2b4533c87eb6e13785124a
Angular CLI <angular-cli@angular.io>
2017-06-25 13:00:25 +0200

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { ScheduleTestPage } from './app.po';
+
+describe('schedule-test App', () => {
+  let page: ScheduleTestPage;
+
+  beforeEach(() => {
+    page = new ScheduleTestPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
